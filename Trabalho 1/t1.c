f9c45e114b6388c7097831de1333f4420b8af9f7
FlammaVulpes <30445877+FlammaVulpes@users.noreply.github.com>
2018-04-06 20:24:16 -0300

    Update t1.c

--- Trabalho 1/t1.c
+++ Trabalho 1/t1.c
@@ -2,8 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define STRING_MAX_SIZE 500
-// Tamanho máximo de uma string
+// Definindo um tamanho máximo para uma string
+// Isso tem nome: preguiça, fiz isso para evitar usar malloc e free inúmeras vezes
+// Achei mais tranquilo trabalhar com ponteiros que apontam para arrays de char do que com alocação dinâmica
+// Isso *potencialmente* vai custar alguma nota
+#define STRING_MAX_SIZE 1000
 
 /*
     Pensando um pouco melhor,
@@ -12,6 +15,7 @@
     Muito tarde para mudar.
 */
 
+// Estrutura para lidar com os arquivos de até 10 IDs
 struct FILES{
     FILE* filePointer;
     char filePath[STRING_MAX_SIZE];
@@ -42,17 +46,23 @@ void closeFile(Files[10], int);
 
 
 int main(int argc, char **argv){
-    FILE *input; // Arquivo de entrada ditado pelo argumento da linha de comando -f [inputDir]
+    // Arquivo de entrada ditado pelo argumento -f da linha de comando
+    FILE *input;
     char dirPath[STRING_MAX_SIZE];
-    char commandLineParameter[3][STRING_MAX_SIZE]; // @1 = [0], @2 = [1]; @3 = [2]
+
+    // Criando as variáveis @1, @2 e @3
+    char commandLineParameter[3][STRING_MAX_SIZE];
 
     // Inicializando os parâmetros @1, @2 e @3 como \0
     for(int i = 0; i < 3; i++){
         commandLineParameter[i][0] = '\0';
     }
 
-    input = fopen(argv[2], "r"); // Abrindo o arquivo de entrada
-    int parameterIndex = 0; // Parâmetros para pegar @1, @2 e @3
+    // Abrindo o arquivo definido pelo argumento -o da minha de comando
+    input = fopen(argv[2], "r");
+
+    // Variável utilitária para pegar os @1, @2 e @3
+    int parameterIndex = 0;
 
     // Verificando se a opção "-o" existe e, posteriormente, pegando os parâmetros
     if(strcmp(argv[2], "-o") == 0){
@@ -109,8 +119,6 @@ void doCommands(FILE *input, char *p1, char *p2, char *p3, char *dirPointer, cha
 
     // Loop para a seleção e realização dos comandos
     while(fscanf(input, "%s", command) != EOF){
-        // Apenas com função de debug
-        printf("%s ", command);
 
         // Funções de concatenação...
         if(strcmp(command, "+x") == 0){
@@ -193,9 +201,6 @@ void concatenateVariableNormal(char **var, FILE *input, char *p1, char *p2, char
     // Retirar o caracter '\n' (new line) do final da string
     toBeConcatenated[strcspn(toBeConcatenated, "\n")] = 0;
 
-    // Apenas para testes, será removida posteriormente
-    printf("%s\n", toBeConcatenated);
-
     // Tamanho da variável (x, y ou z) para posterior utilização na alocação de memória
     int varSize = strlen(*var);
 
@@ -206,9 +211,6 @@ void concatenateVariableNormal(char **var, FILE *input, char *p1, char *p2, char
     if(strcmp(toBeConcatenated, "@1") == 0){
         // Tamanho para alocação de memória posterior
         int pSize = strlen(p1);
-
-        // Temporário, será retirado mais tarde
-        printf("varSize: %d \tpSize: %d\n", varSize, pSize);
 
         // Faz uma cópia da variável
         strcpy(dummyString, *var);
@@ -222,30 +224,25 @@ void concatenateVariableNormal(char **var, FILE *input, char *p1, char *p2, char
         strcat(*var, p1);
 
         // Mais uma vez, um testezinho
-        printf("Test: %s\n", *var);
 
         // Por fim, retorna
         return;
     // E o mesmo processo é repetido duas vezes
     } else if(strcmp(toBeConcatenated, "@2") == 0){
         int pSize = strlen(p2);
-        printf("varSize: %d \tpSize: %d\n", varSize, pSize);
         strcpy(dummyString, *var);
         free(*var);
         *var = malloc((pSize + varSize + 1) * sizeof(char));
         strcpy(*var, dummyString);
         strcat(*var, p2);
-        printf("Test: %s\n", *var);
         return;
     } else if(strcmp(toBeConcatenated, "@3") == 0){
         int pSize = strlen(p3);
-        printf("varSize: %d \tpSize: %d\n", varSize, pSize);
         strcpy(dummyString, *var);
         free(*var);
         *var = malloc((pSize + varSize + 1) * sizeof(char));
         strcpy(*var, dummyString);
         strcat(*var, p3);
-        printf("Test: %s\n", *var);
         return;
     // Se passar por aqui, quer dizer que caímos em uma string delimitada por aspas duplas
     } else {
@@ -260,7 +257,6 @@ void concatenateVariableNormal(char **var, FILE *input, char *p1, char *p2, char
         *var = malloc((pSize + varSize + 1) * sizeof(char));
         strcpy(*var, dummyString);
         strcat(*var, toBeConcatenated);
-        printf("Test: %s\n", *var);
         return;
     }
 }
@@ -269,19 +265,20 @@ void concatenateVariableNormal(char **var, FILE *input, char *p1, char *p2, char
 void concatenateVariableSpecial(char **var, FILE *input, char *p1){
     char removeLeadingWhitespace = fgetc(input);
 
+    // Limpar o caracter \n
     char toBeConcatenated[STRING_MAX_SIZE];
     fgets(toBeConcatenated, STRING_MAX_SIZE, input);
-    toBeConcatenated[strcspn(toBeConcatenated, "\n")] = 0; // Limpar o caracter \n
+    toBeConcatenated[strcspn(toBeConcatenated, "\n")] = 0;
 
-    printf("%s\n", toBeConcatenated);
-
+    // Mais ou menos o mesmo processo de antes aqui se repete
     int varSize = strlen(*var);
     char dummyString[STRING_MAX_SIZE];
 
     if(strcmp(toBeConcatenated, "**") == 0){
+        // Concatenar @1 vezes
         strcpy(dummyString, *var);
         int n = atoi(p1);
-        free(var);
+        free(*var);
         *var = malloc(((n * varSize) + 1) * sizeof(char));
         for(int i = 0; i < n; i++){
             if(i == 0){
@@ -291,9 +288,15 @@ void concatenateVariableSpecial(char **var, FILE *input, char *p1){
             }
         }
     } else if(toBeConcatenated[0] == '*'){
+        // Concatenar *n vezes
         strcpy(dummyString, *var);
-        int n = ctoi(toBeConcatenated[1]);
-        free(var);
+
+        // Ponteiro para modificar um array de chars e coletar a quantidade de vezes
+        char *catPointer = toBeConcatenated;
+        removeCharFromString(&catPointer, '*');
+        int n = atoi(catPointer);
+
+        free(*var);
         *var = malloc(((n * varSize) + 1) * sizeof(char));
         for(int i = 0; i < n; i++){
             if(i == 0){
@@ -303,22 +306,25 @@ void concatenateVariableSpecial(char **var, FILE *input, char *p1){
             }
         }
     } else {
-        // Função para remover as aspas de uma string
+        // Concatenar uma string delimitada por duas aspas
         char *p = toBeConcatenated;
         removeCharFromString(&p, '\"');
+
         int pSize = strlen(toBeConcatenated);
         strcpy(dummyString, *var);
         free(*var);
         *var = malloc((pSize + varSize + 1) * sizeof(char));
         strcpy(*var, dummyString);
         strcat(*var, toBeConcatenated);
-        printf("Test: %s\n", *var);
         return;
     }
 }
 
 void clearVariable(char **var){
+    // Limpando a variável
     free(*var);
+
+    // Alocando espaço para o caracter '\0'
     *var = malloc(sizeof(char));
     strcpy(*var, "");
 }
@@ -330,6 +336,7 @@ void setCurrentDirectory(FILE* input, char **path, char **defaultPath, char *x, 
     fgets(dirArg, STRING_MAX_SIZE, input);
     dirArg[strcspn(dirArg, "\n")] = 0;
 
+    // Seleciona o path a partir de uma variável ou de uma string com um path
     if(strcmp(dirArg, "#x") == 0){
         strcpy(*path, x);
     } else if(strcmp(dirArg, "#y") == 0){
@@ -339,9 +346,8 @@ void setCurrentDirectory(FILE* input, char **path, char **defaultPath, char *x, 
     } else {
         strcpy(*path, dirArg);
     }
-    // Função para remover ocorrências de barras duplas ("//")
-    removeCharFromString(path, '.');
-    printf("%s\n", *path);
+    // Adicionando um (potencial) segundo '/' ao final do path
+    strcat(*path, "/");
 }
 
 void setFileName(char **fileName, FILE* input, char *x, char *y, char *z){
@@ -358,11 +364,11 @@ void setFileName(char **fileName, FILE* input, char *x, char *y, char *z){
     } else if(strcmp(nameArg, "#z") == 0){
         strcpy(*fileName, z);
     } else {
+        // Mais uma vez, um ponteiro para modificar um array de char
         char *argPointer = nameArg;
         removeCharFromString(&argPointer, '\"');
         strcpy(*fileName, argPointer);
     }
-    printf("%s\n", *fileName);
 }
 
 void setExtension(char **fileExt, FILE* input, char *x, char *y, char *z){
@@ -379,47 +385,52 @@ void setExtension(char **fileExt, FILE* input, char *x, char *y, char *z){
     } else if(strcmp(extArg, "#z") == 0){
         strcpy(*fileExt, z);
     } else {
+        // E de novo...
         char *extPointer = extArg;
         removeCharFromString(&extPointer, '\"');
         strcpy(*fileExt, extPointer);
     }
-    printf("%s\n", *fileExt);
 }
 
 void createFile(Files f[10], int id, char **path, char **fileName, char **extName){
     char dummyString[STRING_MAX_SIZE];
 
+    // Concatenando TUDO para ter o path real desse arquivo
+    // Detalhe: ele não cria os diretórios, ou seja, O DIRETÓRIO JÁ DEVE EXISTIR no momento em que essa função for rodada
     strcpy(dummyString, *path);
     strcat(dummyString, *fileName);
     strcat(dummyString, ".");
     strcat(dummyString, *extName);
 
+    // Abrindo para escrita para conseguir criar o arquivo...
     f[id].filePointer = fopen(dummyString, "w");
+    /*  // Bloco de código para informar um erro, caso existir. O único erro que deve poder surgir é o de arquivo não encontrado devido aos diretórios ainda não existirem
     if(!f[id].filePointer){
         perror("fopen");
-    }
+    } */
     strcpy(f[id].filePath, dummyString);
-    printf("%d (Path: \"%s\")\n", id, f[id].filePath);
 }
 
 void openFile(Files f[10], int id, char **path, char **fileName, char **extName){
     char dummyString[STRING_MAX_SIZE];
+
     strcpy(dummyString, *path);
     strcat(dummyString, *fileName);
     strcat(dummyString, ".");
     strcat(dummyString, *extName);
 
+    // Abre para inclusão - adição de strings ao final do arquivo
     f[id].filePointer = fopen(dummyString, "a");
     strcpy(f[id].filePath, dummyString);
-    printf("%d %s\n", id, f[id].filePath);
 }
 
 void readFile(Files f[10], int id){
+    // Apenas abre para leitura
     f[id].filePointer = fopen(f[id].filePath, "r");
-    printf("%d\n", id);
 }
 
 void writeFile(FILE* input, Files f[10], int id, char **x, char **y, char **z){
+    // Conforme consta no arquivo do trabalho, jogando um erro no console no caso de um arquivo a ser lido não existir.
     if(fopen(f[id].filePath, "w") == NULL){
         printf("Erro: nao existe um arquivo aberto no id %d!\n", id);
         exit(EXIT_FAILURE);
@@ -428,9 +439,9 @@ void writeFile(FILE* input, Files f[10], int id, char **x, char **y, char **z){
 
         char toBeWritten[STRING_MAX_SIZE];
         fgets(toBeWritten, STRING_MAX_SIZE, input);
-        toBeWritten[strcspn(toBeWritten, "\n")] = 0; // Limpar o caracter \n
-        printf("s\n", toBeWritten);
+        toBeWritten[strcspn(toBeWritten, "\n")] = 0;
 
+        // Ponteiros duplos desnecessários
         if(strcmp(toBeWritten, "#x") == 0){
             fprintf(f[id].filePointer, "%s", *x);
         } else if(strcmp(toBeWritten, "#y") == 0){
@@ -438,7 +449,8 @@ void writeFile(FILE* input, Files f[10], int id, char **x, char **y, char **z){
         } else if(strcmp(toBeWritten, "#z") == 0){
             fprintf(f[id].filePointer, "%s", *z);
         } else {
-            toBeWritten[strcspn(toBeWritten, "$")] = 0; // Limpar o caracter $
+            // Limpando o $ ao final da string
+            toBeWritten[strcspn(toBeWritten, "$")] = 0;
             fprintf(f[id].filePointer, "%s", toBeWritten);
         }
     }
@@ -449,7 +461,7 @@ void readLineFromFile(FILE* input, Files f[10], int id, char **x, char **y, char
 
     char toBeWritten[STRING_MAX_SIZE];
     fgets(toBeWritten, STRING_MAX_SIZE, input);
-    toBeWritten[strcspn(toBeWritten, "\n")] = 0; // Limpar o caracter \n
+    toBeWritten[strcspn(toBeWritten, "\n")] = 0;
 
     if(strcmp(toBeWritten, "#x") == 0){
         strcpy(*x, toBeWritten);
@@ -461,8 +473,11 @@ void readLineFromFile(FILE* input, Files f[10], int id, char **x, char **y, char
 }
 
 void closeFile(Files f[10], int id){
+    // Fecha o arquivo
     fclose(f[id].filePointer);
-    printf("%d\n", id);
+
+    // Faz o filePointer desse ID apontar para NULL até que seja criado/aberto outro arquivo nesse mesmo ID
+    f[id].filePointer = NULL;
 }
 
 ///////////////////////////////////
@@ -470,36 +485,18 @@ void closeFile(Files f[10], int id){
 ///////////////////////////////////
 
 void removeCharFromString(char **str, char c){
+    // Remover um determinado char de alguma string
     char *pRead = *str, *pWrite = *str;
     while(*pRead){
         *pWrite = *pRead++;
+        // Condicional que faz essa função funcionar
         pWrite += (*pWrite != c);
     }
+    // Força a inclusão de um null para que a string termine n ocorrências antes
     *pWrite = '\0';
 }
 
 int ctoi(char c){
+    // Função que acabou não possuindo uso
     return c - '0';
-}
-
-void mkpath(char *path){
-    char p[STRING_MAX_SIZE];
-    char pFull[STRING_MAX_SIZE];
-
-    strcpy(p, path);
-    char *pPointer = strtok(p, "/")
-
-    int c = 0;
-
-    while(pPointer != NULL){
-        if(c != 1){
-            c == 1;
-            strcpy(pFull, p);
-        } else {
-            strcat(pFull, "/");
-            strcat(pFull, p);
-        }
-        //
-    }
-    strcpy(p, path);
 }
